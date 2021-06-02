c39a6e6cb1c1c5fe81b85a472da1b31f54220392
antonio-cortes-perez <antonio.cortes.perez@gmail.com>
2021-06-02 10:30:09 -0700

    Fixing bug in matMul4x4.

--- inputs/MatMul4x4.c
+++ inputs/MatMul4x4.c
@@ -3,7 +3,7 @@ void matMul4x4(int *A, int *B, int *C) {
   for (int IdxA = 0; IdxA < Size; ++IdxA) {
     for (int IdxB = 0; IdxB < Size; ++IdxB) {
       int *RowA = A + IdxA * Size;
-      int *ColB = A + IdxB;
+      int *ColB = B + IdxB;
       *C = 0;
       for (int Idx = 0; Idx < Size; ++Idx, ++RowA, ColB += Size) {
         *C += *RowA * *ColB;
